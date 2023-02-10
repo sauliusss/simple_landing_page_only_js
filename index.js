0c0f739231e5b54f9c8840db6e76e5e8ce4cfd3a
saulius <sjsaulius@gmail.com>
2023-02-10 13:08:59 +0200

    add header meniu

--- index.js
+++ index.js
@@ -6,5 +6,29 @@ document.body.appendChild(header);
 const logo = document.createElement("div");
 logo.classList.add("logo");
 document.querySelector("body").appendChild(logo);
-logo.innerHTML = "new logo here";
 header.append(logo);
+logo.innerHTML = "new logo here";
+// add nav class
+const nav = document.createElement("div");
+nav.classList.add("nav");
+header.append(nav);
+// add links in nav
+const homeLink = document.createElement("a");
+const storiesLink = document.createElement("a");
+const libraryLink = document.createElement("a");
+const getStartedLink = document.createElement("a");
+// set attributes
+homeLink.setAttribute("href", "#");
+storiesLink.setAttribute("href", "#");
+libraryLink.setAttribute("href", "#");
+getStartedLink.setAttribute("href", "#");
+// add to nav
+nav.append(homeLink);
+nav.append(storiesLink);
+nav.append(libraryLink);
+nav.append(getStartedLink);
+// add element name
+homeLink.innerHTML = "Home";
+storiesLink.innerHTML = "Stories";
+libraryLink.innerHTML = "Library";
+getStartedLink.innerHTML = "Get Started";
