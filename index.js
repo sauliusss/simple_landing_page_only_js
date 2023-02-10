2df91e6eedc11fdd4908dbf4cd0111623a805e9c
saulius <sjsaulius@gmail.com>
2023-02-10 11:52:40 +0200

    add header and logo class

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+// add header class
 const header = document.createElement("div");
 header.classList.add("header");
-document.body.appendChild.header;
-console.log(header);
+document.body.appendChild(header);
+// add logo class
+const logo = document.createElement("div");
+logo.classList.add("logo");
+document.querySelector("body").appendChild(logo);
+logo.innerHTML = "new logo here";
+header.append(logo);
